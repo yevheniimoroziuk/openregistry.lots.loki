8ccc8af162ae750e10901cdb9814d0da22480fa0
andrii-shchudlo <andriis@quintagroup.org>
2017-08-01 15:04:45 +0300

    Refactor views

---
+++ openregistry/lots/basic/views/lot.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from openregistry.api.utils import (
+    json_view,
+    context_unpack,
+    APIResource
+)
+
+from openregistry.lots.core.utils import (
+    save_lot, oplotsresource, apply_patch
+)
+
+from openregistry.lots.core.validation import (
+    validate_patch_lot_data,
+    validate_lot_status_update_in_terminated_status
+)
+
+
+patch_lot_validators = (validate_patch_lot_data, validate_lot_status_update_in_terminated_status, )
+
+@oplotsresource(name='basic:Lot',
+                  path='/lots/{lot_id}',
+                  lotType='basic',
+                  description="Open Contracting compatible data exchange format.")
+class AssetResource(APIResource):
+
+    @json_view(permission='view_lot')
+    def get(self):
+        lot_data = self.context.serialize(self.context.status)
+        return {'data': lot_data}
+
+    @json_view(content_type="application/json", validators=patch_lot_validators, permission='edit_lot')
+    def patch(self):
+        lot = self.context
+        apply_patch(self.request, src=self.request.validated['lot_src'])
+        self.LOGGER.info(
+            'Updated lot {}'.format(lot.id),
+            extra=context_unpack(self.request, {'MESSAGE_ID': 'lot_patch'})
+        )
+        return {'data': lot.serialize(lot.status)}
