c1738fcedc8fc2b1dddedf6cdf25aa0d1429bb82
yarsanich <nezn27@gmail.com>
2017-08-01 17:56:39 +0300

    WIP: Basic tests

--- openregistry/lots/basic/subscribers/event_handlers.py
+++ openregistry/lots/basic/subscribers/event_handlers.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from pyramid.events import subscriber
-from openregistry.lots.core.events import lotInitializeEvent
+from openregistry.lots.core.events import LotInitializeEvent
 from openregistry.api.utils import get_now
 
 
-@subscriber(lotInitializeEvent, lotType="basic")
+@subscriber(LotInitializeEvent, lotType="basic")
 def tender_init_handler(event):
     """ initialization handler for basic lots """
     event.lot.date = get_now()
