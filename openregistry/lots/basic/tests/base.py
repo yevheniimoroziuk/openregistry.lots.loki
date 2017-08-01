61bcfd73e50042a35c23b6c3d2d30261a999407d
yarsanich <nezn27@gmail.com>
2017-08-01 18:08:14 +0300

    update base data with Organiation

--- openregistry/lots/basic/tests/base.py
+++ openregistry/lots/basic/tests/base.py
@@ -3,10 +3,34 @@ from openregistry.lots.core.tests.base import (
     BaseLotWebTest as BaseLWT
 )
 
+
+test_organization = {
+    "name": u"Державне управління справами",
+    "identifier": {
+        "scheme": u"UA-EDR",
+        "id": u"00037256",
+        "uri": u"http://www.dus.gov.ua/"
+    },
+    "address": {
+        "countryName": u"Україна",
+        "postalCode": u"01220",
+        "region": u"м. Київ",
+        "locality": u"м. Київ",
+        "streetAddress": u"вул. Банкова, 11, корпус 1"
+    },
+    "contactPoint": {
+        "name": u"Державне управління справами",
+        "telephone": u"0440000000"
+    }
+}
+
+test_lotCustodian = test_organization.copy()
+
 test_lot_data = {
     "title": u"Тестовий лот",
     "description": u"Щось там тестове",
     "lotType": "basic",
+    "lotCustodian": test_lotCustodian,
 }
 
 
