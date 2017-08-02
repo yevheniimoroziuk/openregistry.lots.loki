09f71e28d3f4a47f2702b2c28a903c8f021e0d0e
andrii-shchudlo <andriis@quintagroup.org>
2017-08-02 16:58:11 +0300

    Refactoring code

--- openregistry/lots/basic/tests/lot.py
+++ openregistry/lots/basic/tests/lot.py
@@ -8,7 +8,6 @@ from openregistry.lots.basic.tests.base import (
     test_lot_data, BaseLotWebTest
 )
 from openregistry.lots.basic.tests.lot_blanks import (
-
     listing,
     get_lot,
     lot_not_found,
@@ -25,7 +24,6 @@ from openregistry.lots.basic.tests.lot_blanks import (
 class LotTest(BaseWebTest):
     initial_data = test_lot_data
     relative_to = os.path.dirname(__file__)
-
     test_simple_add_lot = snitch(simple_add_lot)
 
 
