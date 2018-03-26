502b915c5c64c9b5a37bbce70234cd9a5c0278e4
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-26 14:57:41 +0300

    Create special constants for ssp lot

--- openregistry/lots/ssp/constants.py
+++ openregistry/lots/ssp/constants.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+LOT_STATUSES = ["draft", "pending", "deleted"]
+
 STATUS_CHANGES = {
     "draft": {
         "editing_permissions": ["lot_owner", "Administrator"],
@@ -10,69 +12,11 @@ STATUS_CHANGES = {
     "pending": {
         "editing_permissions": ["lot_owner", "Administrator"],
         "next_status": {
-            "deleted": ["lot_owner", "Administrator"],
-            "verification": ["lot_owner", "Administrator"]
+            "deleted": ["lot_owner", "Administrator"]
         }
     },
     "deleted": {
         "editing_permissions": [],
         "next_status": {}
     },
-    "verification": {
-        "editing_permissions": ["concierge", "Administrator"],
-        "next_status": {
-            "pending": ["concierge", "Administrator"],
-            "active.salable": ["concierge", "Administrator"]
-        }
-    },
-    "recomposed": {
-        "editing_permissions": ["concierge", "Administrator"],
-        "next_status": {
-            "pending": ["concierge", "Administrator"],
-        }
-    },
-    "active.salable": {
-        "editing_permissions": ["convoy", "Administrator", "lot_owner"],
-        "next_status": {
-            "pending.dissolution": ["lot_owner", "Administrator"],
-            "recomposed": ["lot_owner", "Administrator"],
-            "active.awaiting": ["convoy", "Administrator"],
-            "verification": ["Administrator"]
-        }
-    },
-    "pending.dissolution": {
-        "editing_permissions": ["concierge", "Administrator"],
-        "next_status": {
-            "dissolved": ["concierge", "Administrator"],
-            "active.salable": ["Administrator"]
-        }
-    },
-    "dissolved": {
-        "editing_permissions": [],
-        "next_status": {}
-    },
-    "active.awaiting": {
-        "editing_permissions": ["convoy", "Administrator"],
-        "next_status": {
-            "active.auction": ["convoy", "Administrator"],
-            "active.salable": ["convoy", "Administrator"]
-        }
-    },
-    "active.auction": {
-        "editing_permissions": ["convoy", "Administrator"],
-        "next_status": {
-            "active.salable": ["convoy", "Administrator"],
-            "pending.sold": ["convoy", "Administrator"]
-        }
-    },
-    "pending.sold": {
-        "editing_permissions": ['concierge',  'Administrator'],
-        "next_status": {
-            'sold': ['concierge',  'Administrator']
-        }
-    },
-    "sold": {
-        "editing_permissions": [],
-        "next_status": {}
-    }
 }
