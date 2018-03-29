3bd2da5e2c4c8e3a498f5aa140bad3ad5c9cc0e4
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-29 15:10:32 +0300

    Move tests to blanks folder

---
+++ openregistry/lots/ssp/tests/blanks/item_blanks.py
@@ -0,0 +1,46 @@
+def create_item_resource(self):
+    response = self.app.post_json('/{}/items'.format(self.resource_id),
+                                  headers=self.access_header,
+                                  params={'data': self.initial_item_data})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+    item_id = response.json["data"]['id']
+    self.assertIn(item_id, response.headers['Location'])
+    self.assertEqual(self.initial_item_data['description'], response.json["data"]["description"])
+    self.assertEqual(self.initial_item_data['quantity'], response.json["data"]["quantity"])
+    self.assertEqual(self.initial_item_data['address'], response.json["data"]["address"])
+
+
+def patch_item(self):
+    response = self.app.post_json('/{}/items'.format(self.resource_id),
+                                  headers=self.access_header,
+                                  params={'data': self.initial_item_data})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+    item_id = response.json["data"]['id']
+    self.assertIn(item_id, response.headers['Location'])
+    self.assertEqual(self.initial_item_data['description'], response.json["data"]["description"])
+    self.assertEqual(self.initial_item_data['quantity'], response.json["data"]["quantity"])
+    self.assertEqual(self.initial_item_data['address'], response.json["data"]["address"])
+
+    response = self.app.patch_json('/{}/items/{}'.format(self.resource_id, item_id),
+        headers=self.access_header, params={
+            "data": {
+                "description": "new item description"
+            }})
+    self.assertEqual(response.status, '200 OK')
+    self.assertEqual(response.content_type, 'application/json')
+    self.assertEqual(item_id, response.json["data"]["id"])
+    self.assertEqual(response.json["data"]["description"], 'new item description')
+
+
+def create_item_resource_invalid(self):
+    pass
+
+
+def patch_item_resource_invalid(self):
+    pass
+
+
+def list_item_resource(self):
+    pass
---
+++ openregistry/lots/ssp/tests/blanks/publication_blanks.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from openprocurement.api.tests.blanks.json_data import (
+    publication_auction_english_data,
+    publication_auction_insider_data
+)
+from copy import deepcopy
+
+
+def create_publication(self):
+    response = self.app.post_json('/{}/publications'.format(self.resource_id),
+                                  headers=self.access_header,
+                                  params={'data': self.initial_publication_data})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+    publication_id = response.json["data"]['id']
+    self.assertIn(publication_id, response.headers['Location'])
+    self.assertEqual(
+        self.initial_publication_data['auctions'][0]['procurementMethodType'],
+        response.json['data']['auctions'][0]['procurementMethodType']
+    )
+    self.assertEqual(
+        self.initial_publication_data['auctions'][1]['procurementMethodType'],
+        response.json['data']['auctions'][1]['procurementMethodType']
+    )
+    self.assertEqual(
+        self.initial_publication_data['auctions'][2]['procurementMethodType'],
+        response.json['data']['auctions'][2]['procurementMethodType']
+    )
+
+def patch_publication(self):
+    pub_eng = deepcopy(publication_auction_english_data)
+    pub_insider = deepcopy(publication_auction_insider_data)
+    response = self.app.post_json('/{}/publications'.format(self.resource_id),
+                                  headers=self.access_header,
+                                  params={'data': self.initial_publication_data})
+    self.assertEqual(response.status, '201 Created')
+    self.assertEqual(response.content_type, 'application/json')
+    publication_id = response.json["data"]['id']
+    self.assertIn(publication_id, response.headers['Location'])
+    pub_eng['tenderingDuration'] = 'P15DT12H'
+
+    response = self.app.patch_json('/{}/publications/{}'.format(self.resource_id, publication_id),
+        headers=self.access_header, params={
+            "data": {
+                "auctions": [
+                    pub_eng,
+                    pub_eng,
+                    pub_insider
+                ]
+            }})
+    self.assertEqual(response.status, '200 OK')
+    self.assertEqual(response.content_type, 'application/json')
+    self.assertEqual(publication_id, response.json["data"]["id"])
+    self.assertEqual(
+        pub_eng['tenderingDuration'],
+        response.json['data']['auctions'][0]['tenderingDuration']
+    )
+    self.assertEqual(
+        pub_eng['tenderingDuration'],
+        response.json['data']['auctions'][1]['tenderingDuration']
+    )
