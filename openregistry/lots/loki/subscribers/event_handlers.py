4dfb73c6078ae7151d955d21255781a8d2183c9b
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-25 16:53:00 +0300

    Automatically create auction for lot after creation

--- openregistry/lots/loki/subscribers/event_handlers.py
+++ openregistry/lots/loki/subscribers/event_handlers.py
@@ -10,15 +10,23 @@ from openregistry.lots.loki.constants import DEFAULT_DUTCH_STEPS
 def tender_init_handler(event):
     """ initialization handler for basic lots """
     event.lot.date = get_now()
-    event.lot.serialize()
-    for auction in event.lot.auctions:
-        auctionParameters_class = event.lot.__class__.auctions.model_class.auctionParameters.model_class
-        auction.auctionParameters = auction.auctionParameters if auction.auctionParameters else auctionParameters_class()
-        if auction.procurementMethodType == 'Loki.english':
+    auction_types = ['sellout.english', 'sellout.english', 'sellout.insider']
+    auction_class = event.lot.__class__.auctions.model_class
+    auctionParameters_class = event.lot.__class__.auctions.model_class.auctionParameters.model_class
+
+    for auction_type, tenderAttempts in zip(auction_types, list(range(1, len(auction_types) + 1))):
+        auction = auction_class()
+        auction.procurementMethodType = auction_type
+        auction.tenderAttempts = tenderAttempts
+        auction.auctionParameters = auctionParameters_class()
+        if auction_type == 'sellout.english':
             auction.auctionParameters.type = 'english'
-        if auction.procurementMethodType == 'Loki.insider':
+        if auction_type == 'sellout.insider':
             auction.auctionParameters.type = 'insider'
-            auction.auctionParameters.dutchSteps = DEFAULT_DUTCH_STEPS if auction.auctionParameters.dutchSteps is None else auction.auctionParameters.dutchSteps
-    if SANDBOX_MODE:
-        for auction in event.lot.auctions[1:]:
-            auction.auctionParameters.procurementMethodDetails = event.lot.auctions[0].auctionParameters.procurementMethodDetails
+            auction.auctionParameters.dutchSteps = DEFAULT_DUTCH_STEPS
+        event.lot.auctions.append(auction)
+
+    # To serialize_auction
+    # if SANDBOX_MODE:
+    #     for auction in event.lot.auctions[1:]:
+    #         auction.auctionParameters.procurementMethodDetails = event.lot.auctions[0].auctionParameters.procurementMethodDetails
